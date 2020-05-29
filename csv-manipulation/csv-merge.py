fa0ada866e49a39c9b694ee95716a130acb819ba
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2020-05-29 10:43:06 +0200

    Added support to match multiple columns for merging

--- csv-manipulation/csv-merge.py
+++ csv-manipulation/csv-merge.py
@@ -2,17 +2,16 @@ from __future__ import print_function
 
 # A script to help you with manipulating CSV-files. This is especially necessary when dealing with 
 # CSVs that have more than 65536 lines because those can not (yet) be opened in Excel or Numbers.
-# This script works with the example wintergames_winners.csv, which is an excerpt from 
-# https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results/#athlete_events.csv
-
+# This script works with two files from ourworldindata.org:
+# https://ourworldindata.org/age-structure and https://ourworldindata.org/gender-ratio
 
 # This script MERGES two CSVs.
 
 #
 # Usage:
 #     - Adjust filenames and delimiters.
-#     - Variable matchColumn: name of the matching column in the first CSV 
-#     - Variable withColumn: name of the matching column in the second CSV 
+#     - Variable matchColumns: names of the matching columns in the first CSV 
+#     - Variable withColumns: names of the matching columns in the second CSV 
 #     - Variable copyColumns: which columns from the second CSV should be copied
 #           to the first. If copyColumns is [], it copies all cloumns except 
 #           what's defined in the variable 'withColumn'
@@ -25,23 +24,36 @@ from __future__ import print_function
 # Change the parameters according to your task:
 
 # Give the name of the CSV file where you want to add columns
-readFileName1 = 'wintergames_winners.csv'                          # <--- Adjust here
+readFileName1 = 'worlddata-median-age.csv'                          # <--- Adjust here
 # What delimiter is used in this CSV? Usually ',' or ';'
-readDelimiter1 = ';'                                               # <--- Adjust here (have a look in your source CSV)
+readDelimiter1 = ','                                                # <--- Adjust here (have a look in your source CSV)
 
 # Give the name of the CSV file that gives additional values
-readFileName2 = 'longitude-latitude.csv'                           # <--- Adjust here
+readFileName2 = 'worlddata-share-population-female.csv'             # <--- Adjust here
 # What delimiter is used in this CSV? Usually ',' or ';'
-readDelimiter2 = ','                                               # <--- Adjust here (have a look in your source CSV)
+readDelimiter2 = ','                                                # <--- Adjust here (have a look in your source CSV)
 
 # The result will be a new CSV file:
-writeFileName = 'wintergames_winners_merged.csv'                   # <--- Adjust here (has to be different than readFileName1)
+writeFileName = 'worlddata_merged.csv'                              # <--- Adjust here (has to be different than readFileName1)
 # You can give a different delimiter for the result.
-writeDelimiter = ';'                                               # <--- Adjust here (';' is usually good)
+writeDelimiter = ','                                                # <--- Adjust here (';' is usually good)
 
-matchColumn = 'NOC'
-withColumn = 'IOC'
-copyColumns = ['latitude', 'longitude']
+matchColumns = ['Code', 'Year']                                     # <--- Adjust here
+withColumns = ['Code', 'Year']                                      # <--- Adjust here
+copyColumns = ['PercentFemale']                                     # <--- Adjust here
+
+
+# # Second example for merging longitude/latitude data to a file with countries
+# readFileName1 = 'wintergames_winners.csv'
+# readDelimiter1 = ';'
+# readFileName2 = 'longitude-latitude.csv'
+# readDelimiter2 = ','
+# writeFileName = 'wintergame_winners_merged.csv'
+# writeDelimiter = ';'
+# matchColumns = ['NOC']
+# withColumns = ['IOC']
+# copyColumns = ['latitude', 'longitude']
+
 
 
 # ----------------------------------------------
@@ -76,7 +88,10 @@ print('----------------------')
 dic = {}
 unique = True
 for row in rows2:
-  key = row[withColumn]
+  key = tuple(row[x] for x in withColumns)
+  # for col in withColumns:
+  #   key = key + row[col] + '__'
+
   if key != '':
     if key in dic:
       unique = False
@@ -84,7 +99,7 @@ for row in rows2:
       dic[key] = row
 
 if (not unique):
-  print('Warning: The column "%s" in the second CSV has duplicate values which is not good for matching.' % withColumn)
+  print('Warning: The columns "%s" in the second CSV has duplicate values which could result in incorrect matching.' % withColumns)
   print('----------------------')
 
 print('Merging')
@@ -97,7 +112,11 @@ for i, row in enumerate(rows1):
     perc = perc + 0.01
 
   values = []
-  val = row[matchColumn]
+  val = tuple(row[x] for x in matchColumns)
+  # for col in matchColumns:
+  #   val = val + row[col] + '__'
+
+
   for key in headings1:
     values.append(row[key])
   for key in copyColumns:
@@ -110,5 +129,12 @@ for i, row in enumerate(rows1):
   writer.writerow(values)
 
 print('\n----------------------')
-print('The following values could not be found in the second CSV, so matching was not possible for every row.')
-print(failed)
+print('%d value(s) could not be found in the second CSV, so matching was not possible for every row.' % len(failed))
+print("These values couldn't be matched:")
+print(failed[:100])
+if (len(failed) > 100):
+  print('... and %d more' % (len(failed) - 100))
+
+
+
+
